ff793f9952f40cf696c1aa26274de050aa2bb13a
Anjali-jkh <103761917+Anjali-jkh@users.noreply.github.com>
2022-09-22 10:33:42 +0530

    Create oper.c

---
+++ oper.c
@@ -0,0 +1,20 @@
+( open para
+) close para
+{ openbrace
+} closebrace
+< lesser
+> greater
+" doublequote ' singlequote
+:colon
+;semicolon
+#preprocessor
++equal
+==assign
+%percentage
+^bitwise
+&reference
+*star
++add
+-sub
+\backslash
+/slash
